72f25ff840e9ff04b00362d2fdd781a7246f0cea
shenzhimin <zhimin.shen@yoho.cn>
2016-10-18 21:42:51 +0800

    我的音乐

--- pages/my/index.js
+++ pages/my/index.js
@@ -1,6 +1,16 @@
 Page({
     data: {},
     onLoad: function() {
+        if (!wx.getStorageSync('favlist')) {
+            wx.setStorageSync('favlist', {
+                '我喜欢的音乐': {
+                    picurl: '',
+                    list: []
+                }
+            });
+        }
+    },
+    onShow: function() {
         var favList = [];
         var data = wx.getStorageSync('favlist');
 
